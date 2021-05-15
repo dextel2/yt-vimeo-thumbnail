1ff0711425bcb649d4eb1f1b9ca4d6e5d5a5abc3
Yash Karanke <dex.papa@gmail.com>
2021-05-15 21:56:14 +0530

    adding test cases

--- dist/youtube/getYouTube.d.ts
+++ dist/youtube/getYouTube.d.ts
@@ -1,10 +1,10 @@
 /**
  * @author Yash Karanke
  * @description There are two types of YouTube video URL
- * 				1. https://www.youtube.com/watch?v=<video_id>&ab_channel=Dhawalplaysd4Dhawalplaysd4
+ * 				1. https://www.youtube.com/watch?v=<video_id>
  * 				2. https://www.youtube.com/watch?v=<video_id>&ab_channel=<channel_name>
  *
- * in order to make this method work, we rely on the `2nd` video link type as it is the latest format for most of the users
+ * this method works on both of them
  * @summary a simple method to get YouTube video Thumnail from YouTube URL
  * @since 1
  * @param {String} url
